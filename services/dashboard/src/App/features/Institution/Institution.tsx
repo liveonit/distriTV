4af2f815c557e8dad12c38619371ac990c122317
liveonit <ibarreotrey@gmail.com>
2023-04-14 00:05:33 -0300

    fix liner in frontend ⚙️

--- services/dashboard/src/App/features/Institution/Institution.tsx
+++ services/dashboard/src/App/features/Institution/Institution.tsx
@@ -18,6 +18,7 @@ import { listInstitutions } from 'src/store/institution/institution.action'
 import { InstitutionT } from 'src/store/institution/institution.type'
 import AddIcon from '@material-ui/icons/Add'
 import DeleteIcon from '@material-ui/icons/Delete';
+
 import InstitutionCreateAndEditModal from './InstitutionCreateAndEditModal'
 import InstitutionDeleteModal from './InstitutionDeleteModal'
 

33585cfcc86abbcb5c3158a3b919e9c3a289fee5
liveonit <ibarreotrey@gmail.com>
2023-05-03 17:03:35 -0300

    fix create television validations 🛠️

--- services/api/src/apiV1/Television/types/CreateTelevisionBody.ts
+++ services/api/src/apiV1/Television/types/CreateTelevisionBody.ts
@@ -3,12 +3,14 @@ import { z } from 'zod';
 export const createTelevisionBody = z.object({
   institutionId: z.number().optional(),
   name: z.string(),
-  ip: z.string(),
-  mac: z.string(),
+  ip: z.string().optional(),
+  mac: z.string().optional(),
   tvCode: z.string().length(6),
-  m2mRelations: z.object({
-    labels: z.array(z.number()).optional()
-  }).optional(),
+  m2mRelations: z
+    .object({
+      labels: z.array(z.number()).optional(),
+    })
+    .optional(),
 });
 
 export type CreateTelevisionBodyType = z.infer<typeof createTelevisionBody>;
--- services/dashboard/src/store/television/television.type.ts
+++ services/dashboard/src/store/television/television.type.ts
@@ -4,18 +4,27 @@ export const televisionSchema = z.object({
   id: z.number().optional(),
   name: z.string().nonempty('Name is required').min(2, 'Name must have at least 2 characters'),
   institutionId: z.number().optional(),
-  ip: z.string().nonempty('Ip is required').min(7, 'Ip must have at least 7 characters'),
-  mac: z.string().nonempty('Mac is required'),
+  ip: z.string().ip({ version: 'v4', message: 'IP requires a format like XXX.XXX.XXX.XXX' }),
+  mac: z
+    .string()
+    .regex(
+      /^[0-9a-f]{1,2}([.:-])(?:[0-9a-f]{1,2}\1){4}[0-9a-f]{1,2}$/,
+      'Mac Addess requieres a format like: a1:b2:c3:d4:e5:f6',
+    ),
   tvCode: z.string().nonempty(),
-  m2mRelations: z.object({
-    labels: z.array(z.number()).optional()
-  }).optional(),
-  institution: z.object({
-    id: z.number(),
-    name: z.string(),
-    city: z.string(),
-    locality: z.string()
-  }).optional()
+  m2mRelations: z
+    .object({
+      labels: z.array(z.number()).optional(),
+    })
+    .optional(),
+  institution: z
+    .object({
+      id: z.number(),
+      name: z.string(),
+      city: z.string(),
+      locality: z.string(),
+    })
+    .optional(),
 })
 
 export type TelevisionT = z.TypeOf<typeof televisionSchema>

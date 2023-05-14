5de172fd2885d2fa75cf78731b29d1744b67a994
martinriani <martiniano13@gmail.com>
2023-05-14 19:39:36 -0300

    device info: add custom stat (processes running, app visible, if content playing)

--- services/mobileDistriTV/app/src/main/java/com/distritv/data/service/DeviceInfoService.kt
+++ services/mobileDistriTV/app/src/main/java/com/distritv/data/service/DeviceInfoService.kt
@@ -1,17 +1,18 @@
 package com.distritv.data.service
 
+import android.app.Activity
+import android.content.Context
+import android.os.*
 import com.distritv.data.model.DeviceInfo
-import android.os.Build
-import android.os.Bundle
-import android.os.Environment
-import android.os.StatFs
 import android.util.Log
-import android.widget.TextView
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
+import com.distritv.DistriTVApp
+import com.distritv.daemon.ContentSchedulingDaemon
+import com.distritv.daemon.GarbageCollectorDaemon
+import com.distritv.daemon.RequestDaemon
+import com.distritv.utils.isServiceRunning
 import java.io.File
 
-class DeviceInfoService {
+class DeviceInfoService(private val context: Context) {
     fun getDeviceInfo(): DeviceInfo {
         getStorageInfo()
         return DeviceInfo("123456")
@@ -60,6 +61,38 @@ class DeviceInfoService {
 
         if (suffix != null) resultBuffer.append(suffix)
         return resultBuffer.toString()
+    }
+
+    /**
+     * @return true if all background services are running,
+     * false otherwise.
+     */
+    private fun allProcessesAreRunning(): Boolean {
+        return (isServiceRunning(RequestDaemon::class.java)
+                && isServiceRunning(ContentSchedulingDaemon::class.java)
+                && isServiceRunning(GarbageCollectorDaemon::class.java))
+    }
+
+    private fun isServiceRunning(serviceClass: Class<*>): Boolean {
+        return isServiceRunning(context, serviceClass)
+    }
+
+    /**
+     * @return true if the app is visible in the foreground,
+     * false otherwise.
+     */
+    private fun appIsVisible(): Boolean {
+        val currentActivity: Activity? =
+            (context.applicationContext as DistriTVApp?)?.getCurrentActivity()
+        return currentActivity != null
+    }
+
+    /**
+     * @return true if any content is currently playing,
+     * false otherwise.
+     */
+    private fun isAnyContentPlaying(): Boolean {
+        return (context.applicationContext as DistriTVApp?)?.isContentCurrentlyPlaying() ?: false
     }
 
     companion object {

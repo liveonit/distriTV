a16f016b26aa0ff066856b570e8b6649657ed5ee
Matias Gonnet <gonnet.matias@gmail.com>
2023-07-14 19:19:16 -0300

    schedules de label

--- services/api/src/apiV1/Television/Television.service.ts
+++ services/api/src/apiV1/Television/Television.service.ts
@@ -3,11 +3,14 @@ import { BaseService } from '@lib/BaseClasses/BaseService';
 import { FindOptionsWhere } from 'typeorm/find-options/FindOptionsWhere';
 import { length } from 'class-validator';
 import { Alert } from '@src/entities/Alert';
+import { Schedule } from '@src/entities/Schedule';
 
 export class TelevisionSvc extends BaseService<Television> {
     public getByTVcode = async (tvCode: string, durationLeft: number) => {
-        return Television.find({relations: ['schedules', 'schedules.content', 'alerts'], where: {tvCode}}).then(televisions => {
+        return Television.find({relations: ['schedules', 'schedules.content', 'alerts', 'labels'], where: {tvCode}}).then(televisions => {
             let result = JSON.parse(JSON.stringify(televisions[0]))
+            
+            // Handle alerts
             if (result.alerts.length > 0) {                
                 if(durationLeft === 0){
                     Alert.delete({id: result.alerts[0].id})
@@ -20,7 +23,14 @@ export class TelevisionSvc extends BaseService<Television> {
                 
             }
             delete result.alerts
-            return result
+
+            // Handle schedules associated to label            
+            return Promise.all(televisions[0].labels?.map(label => {
+                return Schedule.find({where: {labelId: label.id}})
+            })!).then(schedules => schedules.flat()).then(flattenSchedules => {
+                result.schedules = result.schedules.concat(flattenSchedules)
+                return result
+            })        
         })
     };
 }

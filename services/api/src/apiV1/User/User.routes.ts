f35b1e52a2c4913e72cf9fddb7b2ad1ee1ee19ad
liveonit <ibarretorey@gmail.com>
2023-07-19 17:20:48 -0300

    fix redirect to login when error 401

--- services/api/src/apiV1/User/User.routes.ts
+++ services/api/src/apiV1/User/User.routes.ts
@@ -4,7 +4,7 @@ import { userController } from './User.controller';
 const routes = new BaseRoute(userController);
 routes.setRoutes({
   getById: { enabled: true, path: '/:id', requiredRoles: ['admin'] },
-  getMany: { enabled: true, path: '/:id', requiredRoles: ['admin'] },
+  getMany: { enabled: true, path: '/', requiredRoles: ['admin'] },
   create: { enabled: true, path: '/', requiredRoles: ['admin'] },
   update: { enabled: true, path: '/', requiredRoles: ['admin'] },
 });
